db4b35197e57a0ae94a35aa4abd0e22832c8e046
ArmenPolymathHovhannisyan <mastermindarmen@gmail.com>
2026-02-27 14:32:31 +0100

    Add project files.

---
+++ SienaTours/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SienaTours.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SienaTours/Models/Tour.cs
@@ -0,0 +1,7 @@
+﻿namespace SienaTours.Models
+{
+    public class Tour
+    {
+
+    }
+}
